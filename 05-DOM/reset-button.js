6b4f8b1f8190e00a5ae84ebfc8aae99f72cc72c8
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2023-07-20 18:13:17 +0530

    records display reset

--- 05-DOM/reset-button.js
+++ 05-DOM/reset-button.js
@@ -1,4 +1,5 @@
-const resetFxn = function () {
+// adding event Listener on Check button
+const displayResetBtn = function () {
   if (usrData.arr.length > 0) {
     console.log("here_____");
 
@@ -7,4 +8,16 @@ const resetFxn = function () {
   }
 };
 
-document.querySelector("#form-btn").addEventListener("click", resetFxn);
+document.querySelector("#form-btn").addEventListener("click", displayResetBtn);
+
+//adding event listener on Reset button
+const removeRecords = function () {
+  if (usrData.arr.length > 0) {
+    usrData.arr = [];
+    document.querySelector(".user-data").innerHTML = "";
+  }
+};
+
+document
+  .querySelector(".result-reset")
+  .addEventListener("click", removeRecords);
